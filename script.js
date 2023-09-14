7896a8277a96228d9bf480b454530305296dc42d
lucio <141471494+lucioscarafiocca@users.noreply.github.com>
2023-09-14 00:23:28 -0300

    finish task missing height and extra tasks

---
+++ script.js
@@ -0,0 +1,59 @@
+function rows(a) {
+    const container = document.querySelector(".container")
+    for(let i=0; i < a; i++) {
+        const div = document.createElement("div")
+        container.appendChild(div)
+        div.setAttribute("style", "padding: 0px;")
+        div.classList.toggle("rows")
+
+    }
+
+}
+
+function rowsInside (a) {
+    const rows = document.querySelectorAll("div.rows")
+    rows.forEach((element) => {
+        for(let i=0; i < a ; i++) {
+            const div = document.createElement("div")
+            element.appendChild(div)
+            div.classList.toggle("in") 
+            div.addEventListener("mouseover", () => div.classList.toggle("change"))
+    
+        }
+        
+    } )
+    
+}
+
+function createDivs(a) {
+    rows(a);
+    rowsInside(a);
+}
+
+const but = document.querySelector(".but")
+but.addEventListener("click", () => { 
+    let answer = prompt("Add amount of squares per side")
+    if ((answer < 101 ) && (answer > 9)) {
+        const rows = document.querySelectorAll(".container")
+        rows.forEach((element) => {
+            element.remove();
+        })
+        const div = document.createElement("div")
+       const body = document.querySelector("body")
+       body.appendChild(div)
+       div.classList.toggle("container");
+       createDivs(answer);
+    } else {
+        return alert("Please input a number between 10 and 100")
+    }
+
+})
+
+console.log(createDivs(10))
+
+
+
+
+
+
+
